b36972d7ff6c3d1dd97a19985bc0e9ea70829868
Freddy Montes <freddymontes@gmail.com>
2019-06-19 19:51:27 -0600

    Add lazyload to markdown images

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-import "prismjs/themes/prism-okaidia.css"
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+import 'prismjs/themes/prism-okaidia.css'
+import 'lazysizes'
