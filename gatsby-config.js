3aedaba3d070666110542c289db7137a67f67db5
Freddy Montes <freddymontes@gmail.com>
2019-07-03 21:45:01 -0600

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
                 background_color: '#ffffff',
                 theme_color: '#663399',
                 display: 'minimal-ui',
-                icon: 'content/assets/gatsby-icon.png',
+                icon: 'content/assets/fmontes-icon.png',
             },
         },
         'gatsby-plugin-offline',
